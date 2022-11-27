d78397ef9d54c43efcbc03eae492da4804c60209
Ross <gh@ross.rlg.io>
2022-11-27 00:51:34 +0000

    ModulesAndFunctions-6 with private functions

--- chapter6/ModulesAndFunctions-6.exs
+++ chapter6/ModulesAndFunctions-6.exs
@@ -1,16 +1,16 @@
 defmodule Chop do
-  def middle(lower..upper) do
+  defp middle(lower..upper) do
     lower + div(upper-lower, 2)
   end
-  def doGuess(actual, guess, lower..upper) when actual in lower..upper and guess == actual do
+  defp doGuess(actual, guess, lower..upper) when actual in lower..upper and guess == actual do
     IO.puts "It is #{guess}! in #{lower}..#{upper}"
     guess
   end
-  def doGuess(actual, guess, lower..upper) when actual in lower..upper and guess < actual do
+  defp doGuess(actual, guess, lower..upper) when actual in lower..upper and guess < actual do
     IO.puts "<Is it #{guess}? in #{lower}..#{upper}"
     doGuess(actual, middle(guess..upper), guess..upper)
   end
-  def doGuess(actual, guess, lower..upper) when actual in lower..upper and guess > actual do
+  defp doGuess(actual, guess, lower..upper) when actual in lower..upper and guess > actual do
     IO.puts ">Is it #{guess}? in #{lower}..#{upper}"
     doGuess(actual, middle(lower..guess), lower..guess)
   end
